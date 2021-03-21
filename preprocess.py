a6411bbc6278160fa931c20909a6b669bb674be5
Dina Chen <d.chen-4@student.tudelft.nl>
2021-03-21 22:12:18 +0100

    Finish color jitter function, to randomly change brightness, saturation, contrast and hue of images.

--- preprocess.py
+++ preprocess.py
@@ -9,13 +9,24 @@ import os
 from pathlib import Path
 import shutil
 
+
 scalar = 1 / 3
+jitter_hue = 0.125
+jitter_contrast = 0.4
+jitter_saturation = 0.4
+jitter_brightness = 0.4
+
 ######################  Data pre-processing  ###########
 # "For both COCO datasets, input images are shrunk
 # by two thirds and cropped to 128 × 128 pixels, 128 * 128"
 
 # image is an numpy.ndarray 640 586 3
 #Tensor Image is a tensor with (C, H, W) shape, where C is a number of channels, H and W are image height and width.
+
+###################### Transformation G  ##############
+# horizontal flip
+# random crop
+# random color changes in hue, saturation and brightness
 
 
 # to scale the image by scalar
@@ -69,14 +80,34 @@ def flipImageFile():
         saveAs = to_data_dir + str(imageId)
         flippedImage.save(saveAs)
 
+# perform color transformation for every image in from_data_dir and save in folder to_data_dir
+# jitter: brightness, contrast, saturation, hue, parameters same as in original IIC code
+def colorJitterImageFile():
+
+    from_data_dir = 'val2017/bears/croppedBears/'
+    to_data_dir = 'val2017/bears/colorJitBears/'
+
+    colorJitter = tvt.transforms.ColorJitter(jitter_brightness, jitter_contrast, jitter_saturation, jitter_hue)
+
+    for imageId in os.listdir(from_data_dir):
+        path = from_data_dir + imageId
+        image = Image.open(path)
+        jitteredImage = colorJitter(image)
+        saveAs = to_data_dir + str(imageId)
+        jitteredImage.save(saveAs)
 
 def main():
+    colorJitterImageFile()
     #im = Image.open("val2017/bears/croppedBears/000000073118.jpg")
     #i = ImageOps.mirror(Image.open('val2017/bears/croppedBears/000000073118.jpg'))
     #i.show()
     #scaleImagesFile()
     #cropImagesFile()
-    flipImageFile()
+    #flipImageFile()
+    #colorJitter = tvt.transforms.ColorJitter(jitter_brightness, jitter_contrast, jitter_saturation, jitter_hue)
+    #jitImage = colorJitter(im)
+    #im.show()
+    #jitImage.show()
 
 main()
 
